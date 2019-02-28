50d0f6e9523e95ebae3b85bfca5f043298122dac
liangyuanyuan <liangyuanyuan@dongqiudi.com>
2019-02-28 19:24:21 +0800

    优化工具，添加设置支持开关

--- app/src/main/java/com/android/edittext/MainActivity.java
+++ app/src/main/java/com/android/edittext/MainActivity.java
@@ -3,6 +3,9 @@ package com.android.edittext;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author liangyanqiao
+ */
 public class MainActivity extends AppCompatActivity {
 
     private MaterialEditText mEditText;
@@ -13,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mEditText = findViewById(R.id.edit_text);
+//        mEditText.setIsUseMaterialEditText(false);
     }
 }
--- app/src/main/java/com/android/edittext/MaterialEditText.java
+++ app/src/main/java/com/android/edittext/MaterialEditText.java
@@ -2,9 +2,11 @@ package com.android.edittext;
 
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -13,7 +15,7 @@ import android.util.AttributeSet;
 /**
  * MaterialEditText
  *
- * @author liangyanqiao
+ * @author liang
  */
 public class MaterialEditText extends android.support.v7.widget.AppCompatEditText {
 
@@ -24,42 +26,55 @@ public class MaterialEditText extends android.support.v7.widget.AppCompatEditTex
     private static final float TEXT_MARGIN = Utils.dp2px(8f);
 
     private boolean isShown;
-    private boolean isUseMaterialEditText;
+    private boolean mIsUseMaterialEditText;
 
     /**
      * 动画执行的进度数值
      */
     private float floatFraction;
 
-    Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Rect mRectPadding = new Rect();
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
 
     public MaterialEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initView();
+        initView(context, attrs);
     }
 
 
-    private void initView() {
+    private void initView(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MaterialEditText);
+        mIsUseMaterialEditText = typedArray.getBoolean(R.styleable.MaterialEditText_useMaterialEditText, true);
+        typedArray.recycle();
+
         mPaint.setTextSize(TEXT_SIZE);
         mPaint.setColor(Color.BLACK);
 
-        setPadding(getPaddingLeft(), (int) (getPaddingTop() + TEXT_MARGIN + TEXT_SIZE), getPaddingRight(), getPaddingBottom());
+        /*mPaint.setStyle(Paint.Style.STROKE);
+         mPaint.setStrokeWidth(Utils.dp2px(1));*/
 
+
+        onMaterialEditTextChanged();
         addTextChangedListener(new OnEditTextChangeListener());
-
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        // 0xff 满值
+
+       /* Rect backRect = getBackground().getBounds();
+        mPaint.setColor(Color.BLUE);
+        canvas.drawRect(backRect, mPaint);
+        mPaint.setColor(Color.BLACK);*/
+
+        /*0xff 满值*/
         mPaint.setAlpha((int) (0xff * floatFraction));
-        // 移动的偏移量
+        /*移动的偏移量*/
         float extraOffSet = TEXT_ANIMATION_OFFSET * (1 - floatFraction);
-        // 向上时 extraOffSet  24.0 ----> 0.0          floatFraction 1->0
-        // 向下时 extraOffSet  0.0 ----> 24.0          floatFraction 0->1
+        /*向上时 extraOffSet  24.0 ----> 0.0          floatFraction 1->0
+          向下时 extraOffSet  0.0 ----> 24.0          floatFraction 0->1*/
 //        System.out.println("floatFraction= " + floatFraction + "   1 - floatFraction = [" + (1 - floatFraction) + "]" + "  extraOffSet = [" + extraOffSet + "]");
         canvas.drawText(getHint().toString(), TEXT_HORIZONTAL_OFFSET, TEXT_VERTICAL_OFFSET + extraOffSet, mPaint);
 
@@ -75,13 +90,14 @@ public class MaterialEditText extends android.support.v7.widget.AppCompatEditTex
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            if (isShown && TextUtils.isEmpty(s)) {
-                isShown = false;
-                getAnimator().reverse();
-            } else if (!isShown && !TextUtils.isEmpty(s)) {
-                isShown = true;
-
-                getAnimator().start();
+            if (mIsUseMaterialEditText) {
+                if (isShown && TextUtils.isEmpty(s)) {
+                    isShown = false;
+                    getAnimator().reverse();
+                } else if (!isShown && !TextUtils.isEmpty(s)) {
+                    isShown = true;
+                    getAnimator().start();
+                }
             }
         }
 
@@ -102,18 +118,18 @@ public class MaterialEditText extends android.support.v7.widget.AppCompatEditTex
 
 
     /**
-     *
-     *  动画显示过，并且没有输入文字时候   执行 hide
-     *  ObjectAnimator mObjectAnimatorHide = ObjectAnimator.ofFloat(MaterialEditText.this, "floatFraction", 0);
-     *  mObjectAnimatorHide.start();
-     *
-     *  动画显示过，并且输入文字时候或有文字   执行 show
-     *  ObjectAnimator mObjectAnimatorShow = ObjectAnimator.ofFloat(MaterialEditText.this, "floatFraction", 1);
-     *  mObjectAnimatorShow.start();
-     *
-     *  上面代码逐渐优化为 getAnimator()
-     *
+     * 动画显示过，并且没有输入文字时候   执行 hide
+     * ObjectAnimator mObjectAnimatorHide = ObjectAnimator.ofFloat(MaterialEditText.this, "floatFraction", 0);
+     * mObjectAnimatorHide.start();
+     * <p>
+     * 动画显示过，并且输入文字时候或有文字   执行 show
+     * ObjectAnimator mObjectAnimatorShow = ObjectAnimator.ofFloat(MaterialEditText.this, "floatFraction", 1);
+     * mObjectAnimatorShow.start();
+     * <p>
+     * 上面代码逐渐优化为 getAnimator()
+     * <p>
      * getAnimator
+     *
      * @return ObjectAnimator animator
      */
     public ObjectAnimator getAnimator() {
@@ -122,10 +138,23 @@ public class MaterialEditText extends android.support.v7.widget.AppCompatEditTex
     }
 
 
-
-    public void setUseMaterialEditText(boolean isUse){
-         this.isUseMaterialEditText = isUse;
-         requestLayout();
+    public void setIsUseMaterialEditText(boolean isUse) {
+        if (mIsUseMaterialEditText != isUse) {
+            this.mIsUseMaterialEditText = isUse;
+            onMaterialEditTextChanged();
+        }
     }
+
+    public void onMaterialEditTextChanged() {
+        // 获取 view 自身的 padding
+        getBackground().getPadding(mRectPadding);
+        if (mIsUseMaterialEditText) {
+            setPadding(getPaddingLeft(), (int) (mRectPadding.top + TEXT_MARGIN + TEXT_SIZE), getPaddingRight(), getPaddingBottom());
+        } else {
+            setPadding(getPaddingLeft(), mRectPadding.top, getPaddingRight(), getPaddingBottom());
+        }
+
+    }
+
 
 }
